8dfc24b2a96fe6a12cbda1e159928e2260c39655
Kia de Paula <115732575+KiaMarins@users.noreply.github.com>
2024-09-04 17:21:34 -0300

    Instruções - certo
    sempre colocar npm install bcrypt e npm install @types/bcrypt --save-dev assim como mudar a senha do BD, criar bd autenticacao, npm install e abrir postman  no email profissional

---
+++ dist/users/users.controller.d.ts
@@ -0,0 +1,11 @@
+import { UserService } from './users.service';
+import { User } from './users.entity';
+export declare class UserController {
+    private readonly userService;
+    constructor(userService: UserService);
+    findAll(): Promise<User[]>;
+    findOne(id: number): Promise<User>;
+    create(user: User): Promise<User>;
+    update(id: number, user: Partial<User>): Promise<User>;
+    remove(id: number): Promise<void>;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async create(user: User): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+ 
+    return this.userRepository.save(user);
+  }
+
+  async update(id: number, user: Partial<User>): Promise<User> {
+    await this.userRepository.update(id, user);
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
